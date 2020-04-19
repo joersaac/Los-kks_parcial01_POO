f9fab5ee608e2d770c5f10400f620adfd49643d3
Axel <00088119@uca.edu.sv>
2020-04-18 22:13:39 -0600

    Add: Class Empleado

---
+++ Ejercicio Parcial 01/src/com/JESA/s00159619/Empleado.java
@@ -0,0 +1,54 @@
+package com.JESA.s00159619;
+
+import java.util.ArrayList;
+
+abstract class Empleado {
+    protected String Nombre;
+    protected String puesto;
+    protected ArrayList<Documento> Documentos;
+    protected double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        Nombre = nombre;
+        this.puesto = puesto;
+        Documentos = new ArrayList<>();
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public ArrayList<Documento> getDocumentos() {
+        return Documentos;
+    }
+
+    public void addDocumento (Documento doc){
+        Documentos.add(doc);
+    }
+
+    public void removeDocumento (String name){
+        Documentos.removeIf(s->s.getNombre().equalsIgnoreCase(name));
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "Nombre='" + Nombre + '\'' +
+                ", puesto='" + puesto + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
