442dc15d139d4d8aa3ac7d79583dc34037778e46
Axel <00088119@uca.edu.sv>
2020-04-19 15:44:14 -0600

    Add: Class SalaryViolationEexception

---
+++ Ejercicio Parcial 01/src/com/JESA/s00159619/SalaryViolationException.java
@@ -0,0 +1,10 @@
+package com.JESA.s00159619;
+
+public class SalaryViolationException extends Exception {
+    public SalaryViolationException(){
+        super();
+    }
+    public SalaryViolationException (String message){
+        super(message);
+    }
+}
