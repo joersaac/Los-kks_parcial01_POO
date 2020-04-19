a83e92935a2a9bda0eaf2cb43a63dd67074c8762
unknown <00159619@uca.edu.sv>
2020-04-19 16:45:56 -0600

    Add: Class NotFoundEmployeeException

---
+++ Ejercicio Parcial 01/src/com/JESA/s00159619/NotFoundEmployeeException.java
@@ -0,0 +1,11 @@
+package com.JESA.s00159619;
+
+public class NotFoundEmployeeException extends Exception{
+    public NotFoundEmployeeException() {
+        super();
+    }
+
+    public NotFoundEmployeeException(String message) {
+        super(message);
+    }
+}
